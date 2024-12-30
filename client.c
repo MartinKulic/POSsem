d0561e55f385cd10a29ec4cd97dcd5bd7ec6e0a7
MartinKulic <kulich@stud.uniza.sk>
2024-12-30 22:20:15 +0100

    Zlepsenie synchronizacie vstupneho "buffera" medzi logic_task a input_task

--- client.c
+++ client.c
@@ -9,18 +9,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
 
 #define PORT 8080
 
 typedef struct comunication_data{
-  pthread_cond_t* cond;
-  _Bool* wait;
-  pthread_mutex_t* lock;
+
 }comunication_data;
 
 void * comunication_task(void* argv)
 {
-  comunication_data* this = argv;
+  //comunication_data* this = argv;
 
   int status, valread, client_fd;
   struct sockaddr_in serv_addr;
@@ -48,52 +47,27 @@ void * comunication_task(void* argv)
     return NULL;
   }
 
-printf("connected\n");
+  printf("connected\n");
 
-  while (*this->wait) {
-   // pthread_cond_wait(this->cond, this->lock);
- // printf("waiting\n");
+
+  for (int i = 0; i < 5; i++)
+  {
+    printf("enter command: ");
+
+
+
+
+    char buffer[100] = {0};
+    scanf("%s", buffer);
+    send(client_fd, &buffer[0], 1, 0);
   }
-
-  send(client_fd, "s", 1, 0);
-
-//  for (int i = 0; i < 5; i++)
-//  {
-//    printf("enter command: ");
-//    char buffer[100] = {0};
-//    scanf("%s", buffer);
-//    send(client_fd, &buffer[0], 1, 0);
-//  }
-  time_t now = time(0);
-  printf("%d\n", now);
   close(client_fd);
   return NULL;
 }
 
 int main (int argc, char* argv[])
 {
-  _Bool wait = 1;
-  pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-  pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-  comunication_data com_data = {&cond, &wait, &lock};
+  comunication_task(NULL);  
 
-  pthread_t * threads = calloc(5, sizeof(pthread_t));
-  
-
-  for (int i = 0; i<3; i++)
-  { 
-    pthread_create(&threads[i], NULL, comunication_task, &com_data);
-  }
-
-  sleep(1);
-  printf("unlocking\n");
-  wait = 0;
-  //pthread_cond_broadcast(&cond);
-
-  for (int i = 0;i<3;i++)
-  {
-    pthread_join(threads[i], NULL);
-  }
-  free(threads);
   return 0;
 }
