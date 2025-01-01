92aa741a515d09ac1318fe87961ee35f7c844b08
MartinKulic <kulich@stud.uniza.sk>
2025-01-01 17:01:40 +0100

    Klient sprave rozoznava vstupy v konzole

--- client.c
+++ client.c
@@ -10,17 +10,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
+#include <poll.h>
 
 #define PORT 8080
 
 typedef struct input_data{
 
 }input_data;
-
-void * connect_to_server (int * client_fd)
+// 1 ak prebehne uspesne 
+int connect_to_server (int * client_fd)
 {
 //input_data* this = argv;
-
   int status;
   struct sockaddr_in serv_addr;
   //char buffer[1024] = {0};
@@ -28,7 +28,7 @@ void * connect_to_server (int * client_fd)
   if((*client_fd=socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
     printf("Socket creation error \n");
-    return NULL;
+    return 0;
   }
 
   serv_addr.sin_family = AF_INET;
@@ -38,20 +38,20 @@ void * connect_to_server (int * client_fd)
   if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
   {
     printf("Invalid address\n");
-    return NULL;
+    return 0;
   }
 
   if ((status = connect(*client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0)
   {
     printf("Connection fail\n");
-    return NULL;
+    return 0;
   }
 
   printf("connected\n");
-
+  return 1;
   
 }
-void * input_task(int client_fd)
+void * communication_task(int client_fd)
 {
   struct termios oldt, newt;
   tcgetattr(0, &oldt);
@@ -60,53 +60,68 @@ void * input_task(int client_fd)
   ////polling read
   //blocking read
   newt.c_cc[VTIME] = 0; // timeot in deciseconds for noncanonical read
- // newt.c_cc[VMIN] = 1;  // minimum number of cahracters for noncanocical read
+  //newt.c_cc[VMIN] = 1;  // minimum number of cahracters for noncanocical read
   tcsetattr(0, TCSANOW, &newt);
   //cfmakeraw();
+
+  struct pollfd fds[1];
+  fds[0].fd = STDIN_FILENO;
+  fds[0].events = POLLIN;
 
   char ch[3];
   while(1)
   {
    // printf("enter command: ");
-    
-    read(STDIN_FILENO, &ch[0], 1);
-    if(ch[0] == '\033')
+ 
+    printf("bfr poll\n");
+    if (poll(fds,1,900)>0)
     {
-      read(STDIN_FILENO, &ch[1], 1);
-      if(ch[1]=='[')
+      if (fds[0].revents & POLLIN)
       {
-        read(STDIN_FILENO, &ch[2], 1);
-        if (ch[2]!=0)
+        read(fds[0].fd, &ch[0], 1);
+        printf("%d   ", ch[0]);
+        if(ch[0]=='\033')
         {
-          char toSend[1]={0};
-          switch(ch[2])
+          if(poll(fds, 1, 5)>0)
           {
-            case 'A':
-              toSend[0] = 94;
-              break;
-            case 'B':
-              toSend[0] = 'V';
-              break;
-            case 'C':
-              toSend[0]= '>';
-              break;
-            case 'D':
-              toSend[0]='<';
-              break;
+            read(fds[0].fd, &ch[1], 1);
+            if(ch[1]=='[')
+            {
+              char toSend[1];
+              if(poll(fds, 1, 5)>0)
+              {
+                read(fds[0].fd, &ch[2], 1);
+                switch(ch[2])
+                {
+                  case 'A':
+                    toSend[0] = 'A';
+                    break;
+                  case 'B':
+                    toSend[0] = 'V';
+                    break;
+                  case 'C':
+                    toSend[0]= '>';
+                    break;
+                  case 'D':
+                    toSend[0]='<';
+                    break;
+                }// 3. char
+                printf("%c\n", toSend[0]);
+                send(client_fd, toSend, 1, 0);
+              }// je 3. char
+            }// 2. char je [
+          }// je 2. char
+          else // nie je 2. char
+          {
+            printf("esc\n");
+            send(client_fd, "q", 1, 0);
+            break;
           }
-          printf("%c\n", toSend[0]);
-          send(client_fd, toSend, 1, 0);
-        }
-        
-      } else
-        {
-          printf("esc\n");
-          break;
-        }
-    } if (ch[0]==27){
-      printf("esc\n");
-      break;
-    }
+        }// 1. char je 27 - \033
+      }// je 1. char na citanie v fd
+    }// ne 0 fd je ready
+    
+    
   }
   tcsetattr(0, TCSANOW, &oldt); 
   close(client_fd);
@@ -116,8 +131,11 @@ void * input_task(int client_fd)
 int main (int argc, char* argv[])
 {
   int client_fd = 0;
-  connect_to_server(&client_fd);
-  input_task(client_fd);  
+  if (connect_to_server(&client_fd) != 1)
+  {
+    return 2;
+  }
+  communication_task(client_fd);  
 
   return 0;
 }
